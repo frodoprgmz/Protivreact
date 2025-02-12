a50ad056ef870b170d3ff9872294ba41e532191b
unknown <oliver.p.szkola@gmail.com>
2025-02-12 08:47:57 +0100

    skibidi4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 import "./css/Home.css";
 function Home() {
   return (
     <div className="container">
-      <div className='content-first'>
+      <div className="content-first">
         <p>Zajmujemy się tworzeniem zaawansowanych</p>
         <p>stron internetowych</p>
-         
+        <div className="see-more">Dowiedz się więcej</div>
       </div>
     </div>
-    
   );
 }
 
