4c2425738824e7eb833a9498bda51ace51e5821b
unknown <oliver.p.szkola@gmail.com>
2025-02-19 12:44:26 +0100

    skibidi8

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import "./css/Home.css";
-import reactLogo from "../img/react-logo.png"; 
-import animeLogo from "../img/anime-logo.png"; 
-import mysqlLogo from "../img/mysql-logo.png"; 
+import reactLogo from "../img/react-logo.png";
+import animeLogo from "../img/anime-logo.png";
+import mysqlLogo from "../img/mysql-logo.png";
+import { Instagram, Facebook, Linkedin, Info } from "lucide-react";
 
 function Home() {
   const [flipped, setFlipped] = useState({
     react: false,
     anime: false,
-    mysql: false
+    mysql: false,
   });
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleFlip = (tech) => {
     setFlipped((prevState) => ({
       ...prevState,
-      [tech]: !prevState[tech]
+      [tech]: !prevState[tech],
     }));
+  };
+
+  const togglePhoneIcons = (e) => {
+    e.stopPropagation();
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -25,10 +32,28 @@ function Home() {
           <p>Zajmujemy się tworzeniem zaawansowanych</p>
           <p>stron internetowych</p>
           <div className="see-more">Dowiedz się więcej</div>
+          <div className="social-media">
+            <Instagram className="social-media-icon" />
+            <Facebook className="social-media-icon" />
+            <Linkedin className="social-media-icon" />
+          </div>
+          <div className={`social-media-on-phone ${isOpen ? "open" : ""}`}>
+            <div className="social-media-icons">
+              {isOpen && (
+                <>
+                  <Instagram className="social-media-icon" />
+                  <Facebook className="social-media-icon" />
+                  <Linkedin className="social-media-icon" />
+                </>
+              )}
+            </div>
+            <Info
+              className="social-media-icon info-icon"
+              onClick={togglePhoneIcons}
+            />
+          </div>
         </div>
       </div>
-
-      {/* Sekcja technologies */}
       <div className="technologies">
         <div
           className={`tech-card ${flipped.react ? "flipped" : ""}`}
@@ -39,11 +64,13 @@ function Home() {
               <img className="technology-logo" src={reactLogo} alt="React" />
             </div>
             <div className="tech-back">
-              React – to narzędzie do budowania stron internetowych, które sprawia, że są one szybkie i dynamiczne. Dzięki Reactowi strona nie musi się odświeżać w całości przy każdej zmianie, co sprawia, że działa płynniej i wygląda nowocześnie.
+              React – to narzędzie do budowania stron internetowych, które
+              sprawia, że są one szybkie i dynamiczne. Dzięki Reactowi strona
+              nie musi się odświeżać w całości przy każdej zmianie, co sprawia,
+              że działa płynniej i wygląda nowocześnie.
             </div>
           </div>
         </div>
-
         <div
           className={`tech-card ${flipped.anime ? "flipped" : ""}`}
           onClick={() => handleFlip("anime")}
@@ -53,11 +80,13 @@ function Home() {
               <img className="technology-logo" src={animeLogo} alt="Anime.js" />
             </div>
             <div className="tech-back">
-              Anime.js – to biblioteka, która pomaga dodawać animacje na stronach internetowych. Dzięki niej można sprawić, że elementy na stronie będą się poruszać, zmieniać kolory czy płynnie pojawiać się i znikać, co sprawia, że strona wygląda atrakcyjniej.
+              Anime.js – to biblioteka, która pomaga dodawać animacje na
+              stronach internetowych. Dzięki niej można sprawić, że elementy na
+              stronie będą się poruszać, zmieniać kolory czy płynnie pojawiać
+              się i znikać, co sprawia, że strona wygląda atrakcyjniej.
             </div>
           </div>
         </div>
-
         <div
           className={`tech-card ${flipped.mysql ? "flipped" : ""}`}
           onClick={() => handleFlip("mysql")}
@@ -67,7 +96,10 @@ function Home() {
               <img className="technology-logo" src={mysqlLogo} alt="MySQL" />
             </div>
             <div className="tech-back">
-              MySQL – to baza danych, czyli miejsce, w którym strona internetowa przechowuje informacje, np. dane użytkowników, produkty w sklepie internetowym czy wpisy na blogu. Dzięki MySQL można szybko i bezpiecznie przechowywać i wyszukiwać te dane.
+              MySQL – to baza danych, czyli miejsce, w którym strona internetowa
+              przechowuje informacje, np. dane użytkowników, produkty w sklepie
+              internetowym czy wpisy na blogu. Dzięki MySQL można szybko i
+              bezpiecznie przechowywać i wyszukiwać te dane.
             </div>
           </div>
         </div>
