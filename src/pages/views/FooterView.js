4dbceb6f1ced405a64c93d60ea8ec0f0a369a4c5
Oliver-Bev <oliver.p.szkola@gmail.com>
2025-02-25 13:40:30 +0100

    footer kolejna czesc

--- src/pages/views/FooterView.js
+++ src/pages/views/FooterView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/footer.css';
+import { Instagram, Facebook, Linkedin, Info, Github  } from "lucide-react";
 const FooterView = ()=>{
 return(
     <div className='footer'>
@@ -15,9 +16,16 @@ return(
                 </div>
                 <div className='right-info'>
                     <p>Strona Główna</p>
+                    
                 </div>
             </div>
-            <div className='up-section-socialmedia'></div>
+            <div className='up-section-socialmedia'>
+                <div id='social-footer'>
+                <a id='Sledz'>Śledź nas na:</a>
+                <div className='social-media-footer'> <Facebook className="social-media-icon" /></div>
+                <div className='social-media-footer'> <Instagram className="social-media-icon" /></div>
+                </div>
+            </div>
         </div>
     </div>
 );
