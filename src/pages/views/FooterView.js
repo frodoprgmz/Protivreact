dca353e4ef54c48e63f74bedc82b3a9588f32314
Oliver-Bev <oliver.p.szkola@gmail.com>
2025-02-26 20:06:07 +0100

    kontakt poprawiony bev

--- src/pages/views/FooterView.js
+++ src/pages/views/FooterView.js
@@ -4,30 +4,7 @@ import '../css/footer.css';
 import { Instagram, Facebook, Linkedin, Info, Github  } from "lucide-react";
 const FooterView = ()=>{
 return(
-    // <div className='footer'>
-    //     <div className='up-section'>
-    //         <div className='up-section-logo'>
-    //            <a className='logotekst'>PROTIV</a>
-    //         </div>
-    //         <div className='up-section-info'>
-    //             <div className='left-info'>
-    //                 <p>O nas</p>
-    //                 <p>Kontakt</p>
-    //             </div>
-    //             <div className='right-info'>
-    //                 <p>Strona Główna</p>
-                    
-    //             </div>
-    //         </div>
-    //         <div className='up-section-socialmedia'>
-    //             <div id='social-footer'>
-    //             <a id='Sledz'>Śledź nas na:</a>
-    //             <div className='social-media-footer'> <Facebook className="social-media-icon" /></div>
-    //             <div className='social-media-footer'> <Instagram className="social-media-icon" /></div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
+
 
     <footer className="footer">
     <div className="footer-container">
